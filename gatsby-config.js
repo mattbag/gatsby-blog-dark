aedde6cbe273ffd896639fa62d0446b1ef5ec7ad
mattbag <mattbag00@gmail.com>
2018-08-25 15:34:56 +1000

    offline in

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
         icon: `src/assets/favi.png`,
       },
     },
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: 'gatsby-plugin-typography',
